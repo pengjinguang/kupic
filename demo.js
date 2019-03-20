640741fb9f85c5c47424b0e5c6964d430858243c
pengjinguang <1668586047@qq.com>
2019-03-20 19:30:06 +0800

    init

---
+++ demo.js
@@ -0,0 +1,13 @@
+var $wrap = $('.wrapper');
+setTimeout(function(){
+    $wrap.removeClass('init');
+},200);
+$('.item').on('click',function(){
+    $(this).addClass('active');
+    $wrap.addClass('wrapper-active');
+});
+$('.close').on('click',function(e){
+    e.stopPropagation();
+    $('.active').removeClass('active');
+    $wrap.removeClass('wrapper-active');
+})
